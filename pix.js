4b2c6c44d7cd3950e422078cfc64d2ca85f978d9
saboreincasa <carlosdinheironet@gmail.com>
2026-03-09 12:48:43 -0300

    Update pix.js

--- pix.js
+++ pix.js
@@ -1,14 +1,7 @@
-function gerarPix(){
+function pagarPix(){
 
-let total = calcularTotal()
+let total=document.getElementById("total").innerText
 
-let chave = "31983391576"
-
-let nome = "SABORE IN CASA"
-
-let codigoPix =
-`00020126580014BR.GOV.BCB.PIX0136${chave}520400005303986540${total}5802BR5920${nome}6009SAOPAULO62070503***6304`
-
-document.getElementById("pixcode").innerText = codigoPix
+alert("Pagamento via Pix\nValor: R$ "+total+"\nChave Pix: 31983391576")
 
 }
