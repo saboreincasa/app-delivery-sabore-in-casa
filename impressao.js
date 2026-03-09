4fae79b12cfe929eff3d3bcb07b3b222966bf442
saboreincasa <carlosdinheironet@gmail.com>
2026-03-09 14:34:08 -0300

    Update impressao.js

--- impressao.js
+++ impressao.js
@@ -1,5 +1,11 @@
-function imprimir(){
+// impressao.js
 
-window.print()
-
+function imprimirPedido() {
+  let conteudo = "<h2>Pedido Sabore in Casa</h2>";
+  let itens = document.getElementById("itensCarrinho").innerHTML;
+  conteudo += itens;
+  let win = window.open("", "PRINT", "height=600,width=800");
+  win.document.write(conteudo);
+  win.document.close();
+  win.print();
 }
