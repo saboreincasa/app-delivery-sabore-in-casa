d4a5354eacff6acbba48bcbe1ceae32e2a4131f9
saboreincasa <carlosdinheironet@gmail.com>
2026-03-09 12:49:09 -0300

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -1,19 +1,5 @@
-function calcularDistancia(origem,destino){
+function calcularEntrega(km){
 
-const service = new google.maps.DistanceMatrixService()
-
-service.getDistanceMatrix({
-
-origins:[origem],
-destinations:[destino],
-travelMode:"DRIVING"
-
-}).then((res)=>{
-
-let distancia = res.rows[0].elements[0].distance.text
-
-console.log("Distância:",distancia)
-
-})
+return km*2
 
 }
