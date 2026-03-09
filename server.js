e60bd5bd32ef265b48cd57ba61f6c10dd6718cba
saboreincasa <carlosdinheironet@gmail.com>
2026-03-09 12:51:01 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require("express")
-const app = express()
+const express=require("express")
 
-app.use(express.static("."))
+const app=express()
 
-app.listen(3000, ()=>{
+app.use(express.static(__dirname))
 
-console.log("Servidor rodando em http://localhost:3000")
+app.listen(3000,()=>{
+
+console.log("Servidor rodando")
 
 })
