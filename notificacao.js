fdba059caee7b7f8fd22bed93a4efe4167ad9dc7
saboreincasa <carlosdinheironet@gmail.com>
2026-03-09 12:50:04 -0300

    Update notificacao.js

--- notificacao.js
+++ notificacao.js
@@ -1,20 +1,9 @@
-{
-"name": "Sabore In Casa",
-"short_name": "Sabore",
-"start_url": "index.html",
-"display": "standalone",
-"background_color": "#111111",
-"theme_color": "#c62828",
-"icons": [
-{
-"src": "imagens/logo.png",
-"sizes": "192x192",
-"type": "image/png"
-},
-{
-"src": "imagens/logo.png",
-"sizes": "512x512",
-"type": "image/png"
+function notificar(msg){
+
+if(Notification.permission==="granted"){
+
+new Notification(msg)
+
 }
-]
+
 }
