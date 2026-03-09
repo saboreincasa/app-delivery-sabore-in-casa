83c471ed020f8eadb84785395b467c5c8c12c99b
saboreincasa <carlosdinheironet@gmail.com>
2026-03-09 14:33:37 -0300

    Update mapa.js

--- mapa.js
+++ mapa.js
@@ -1,5 +1,20 @@
-function calcularEntrega(km){
+// mapa.js
 
-return km*2
+// Função simples para simular localização de entrega
+function mostrarEntrega() {
+  const mapaDiv = document.getElementById("mapa");
+  if (!mapaDiv) {
+    const div = document.createElement("div");
+    div.id = "mapa";
+    div.style.height = "200px";
+    div.style.background = "#eee";
+    div.innerText = "Mapa do entregador (simulado)";
+    document.body.appendChild(div);
+  }
+}
 
+// Chamar quando pedido for finalizado
+function pedidoSaiuParaEntrega() {
+  mostrarEntrega();
+  notificar("Seu pedido está a caminho!");
 }
