f9ca513f2773179c1bfe45e606384cd985077e01
saboreincasa <carlosdinheironet@gmail.com>
2026-03-09 12:15:53 -0300

    Update notificacao.js

--- notificacao.js
+++ notificacao.js
@@ -1,12 +1,20 @@
-const notifier = require("node-notifier")
-
-function novoPedido(){
-
-notifier.notify({
-
-title:"Novo Pedido",
-message:"Pedido recebido"
-
-})
-
+{
+"name": "Sabore In Casa",
+"short_name": "Sabore",
+"start_url": "index.html",
+"display": "standalone",
+"background_color": "#111111",
+"theme_color": "#c62828",
+"icons": [
+{
+"src": "imagens/logo.png",
+"sizes": "192x192",
+"type": "image/png"
+},
+{
+"src": "imagens/logo.png",
+"sizes": "512x512",
+"type": "image/png"
+}
+]
 }
