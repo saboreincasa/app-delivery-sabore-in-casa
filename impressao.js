5dba425e7c1e23ea7c5dd6ebf91181b83d1de635
saboreincasa <carlosdinheironet@gmail.com>
2026-03-09 12:49:40 -0300

    Update impressao.js

--- impressao.js
+++ impressao.js
@@ -1,20 +1,5 @@
-const escpos = require("escpos")
+function imprimir(){
 
-function imprimirPedido(pedido){
-
-const device = new escpos.USB()
-
-const printer = new escpos.Printer(device)
-
-device.open(()=>{
-
-printer
-.text("SABORE IN CASA")
-.text("Novo Pedido")
-.text(JSON.stringify(pedido))
-.cut()
-.close()
-
-})
+window.print()
 
 }
