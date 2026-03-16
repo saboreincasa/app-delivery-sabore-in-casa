1ce2715f616531546cbbb8d13281a040e3b6094b
saboreincasa <carlosdinheironet@gmail.com>
2026-03-16 14:53:02 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,31 @@
+const CACHE = "sabore-v1"
+
+const arquivos = [
+"/",
+"/index.html",
+"/style.css",
+"/app.js",
+"/produtos.json"
+]
+
 self.addEventListener("install",e=>{
 
-console.log("App instalado");
+e.waitUntil(
 
-});
+caches.open(CACHE)
+.then(cache => cache.addAll(arquivos))
+
+)
+
+})
 
 self.addEventListener("fetch",e=>{
 
-});
+e.respondWith(
+
+caches.match(e.request)
+.then(res => res || fetch(e.request))
+
+)
+
+})
