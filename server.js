0af82d471960b3f9422fc353b96b498877987103
saboreincasa <carlosdinheironet@gmail.com>
2026-03-16 12:41:30 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
-// server.js
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const fs = require("fs");
+const express=require("express")
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
+const app=express()
 
-const PORT = 3000;
+app.use(express.static(__dirname))
 
-// Endpoint para receber pedidos
-app.post("/pedido", (req, res) => {
-  const pedido = req.body;
-  // Salvar pedido em JSON local (simulando banco)
-  fs.readFile("pedidos.json", (err, data) => {
-    let pedidos = [];
-    if (!err) pedidos = JSON.parse(data);
-    pedidos.push(pedido);
-    fs.writeFile("pedidos.json", JSON.stringify(pedidos, null, 2), () => {
-      res.json({ status: "Pedido recebido com sucesso!" });
-    });
-  });
-});
+app.listen(3000,()=>{
 
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+console.log("Servidor rodando")
+
+})
