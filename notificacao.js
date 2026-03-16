4cde22e896787891c6c2707edbc82ab30e175b7f
saboreincasa <carlosdinheironet@gmail.com>
2026-03-16 12:40:38 -0300

    Update notificacao.js

--- notificacao.js
+++ notificacao.js
@@ -1,20 +1,15 @@
-// notificacao.js
+if(Notification.permission!=="granted"){
 
-function notificar(msg) {
-  if ("Notification" in window) {
-    if (Notification.permission === "granted") {
-      new Notification("Sabore in Casa", { body: msg, icon: "imagens/logo.png" });
-    } else if (Notification.permission !== "denied") {
-      Notification.requestPermission().then(permission => {
-        if (permission === "granted") new Notification("Sabore in Casa", { body: msg, icon: "imagens/logo.png" });
-      });
-    }
-  } else {
-    alert(msg);
-  }
+Notification.requestPermission()
+
 }
 
-// Exemplo: notificação de pedido recebido
-function pedidoRecebido() {
-  notificar("Seu pedido foi recebido! 🍕");
+function notificar(msg){
+
+if(Notification.permission==="granted"){
+
+new Notification(msg)
+
+}
+
 }
