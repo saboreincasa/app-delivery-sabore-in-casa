295ab8457c8feb611263af8886158e21a36e1e4d
saboreincasa <carlosdinheironet@gmail.com>
2026-03-17 19:09:54 -0300

    Update pizza.js

--- pizza.js
+++ pizza.js
@@ -1,68 +1,20 @@
+```javascript
 let pizzas=[
 
-{
-nome:"Calabresa",
-ingredientes:"Mussarela, calabresa artesanal, cebola e orégano",
-preco:39.90
-},
-
-{
-nome:"Frango Catupiry",
-ingredientes:"Frango desfiado, catupiry e mussarela",
-preco:42.90
-},
-
-{
-nome:"Portuguesa",
-ingredientes:"Presunto, ovo, cebola e ervilha",
-preco:44.90
-},
-
-{
-nome:"Quatro Queijos",
-ingredientes:"Mussarela, parmesão, gorgonzola e catupiry",
-preco:46.90
-},
-
-{
-nome:"Marguerita",
-ingredientes:"Tomate, mussarela e manjericão",
-preco:41.90
-},
-
-{
-nome:"Pepperoni",
-ingredientes:"Pepperoni americano e mussarela",
-preco:48.90
-},
-
-{
-nome:"Milho com Bacon",
-ingredientes:"Milho verde, bacon e mussarela",
-preco:43.90
-},
-
-{
-nome:"Carne Seca",
-ingredientes:"Carne seca desfiada, cebola roxa e catupiry",
-preco:49.90
-},
-
-{
-nome:"Bacon Cheddar",
-ingredientes:"Bacon crocante e cheddar cremoso",
-preco:47.90
-},
-
-{
-nome:"Pizza do Chef",
-ingredientes:"Molho especial da casa, mussarela premium, picanha fatiada, cebola caramelizada e catupiry",
-preco:54.90
-}
+"Calabresa",
+"Frango Catupiry",
+"Portuguesa",
+"Quatro Queijos",
+"Marguerita",
+"Pepperoni",
+"Milho Bacon",
+"Carne Seca",
+"Bacon Cheddar",
+"Pizza do Chef"
 
 ]
 
-let lista=document.getElementById("lista-pizzas")
+let lista=document.getElementById("listaPizzas")
 
 pizzas.forEach(p=>{
 
@@ -70,17 +22,17 @@ lista.innerHTML+=`
 
 <div class="card">
 
-<h3>${p.nome}</h3>
+<div class="card-content">
 
-<p>${p.ingredientes}</p>
+<h3>${p}</h3>
 
-<p>R$ ${p.preco}</p>
+<button onclick="montar('${p}')">
 
-<button onclick="escolher('${p.nome}',${p.preco})">
-
-Escolher
+Montar pizza
 
 </button>
+
+</div>
 
 </div>
 
@@ -88,46 +40,11 @@ Escolher
 
 })
 
-function escolher(nome,preco){
+function montar(nome){
 
-document.getElementById("opcoes").innerHTML=`
+localStorage.setItem("pizzaEscolhida",nome)
 
-<h2>${nome}</h2>
-
-<label>
-
-<input type="radio" name="tipo">
-
-Pizza Inteira
-
-</label>
-
-<label>
-
-<input type="radio" name="tipo">
-
-Meio a Meio
-
-</label>
-
-<h3>Borda</h3>
-
-<select>
-
-<option>Sem borda</option>
-
-<option>Borda Catupiry +10</option>
-
-<option>Borda Cheddar +10</option>
-
-</select>
-
-<button>
-
-Adicionar ao Carrinho
-
-</button>
-
-`
+window.location="montar-pizza.html"
 
 }
+```
